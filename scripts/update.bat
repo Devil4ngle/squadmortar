8fb9f32d66f7b541660c548aa7881acfe9b0204e
Devil4ngle <101042789+Devil4ngle@users.noreply.github.com>
2023-12-26 11:46:47 +0100

    Update update.bat

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,6 +1,7 @@
 @echo off
 timeout /t 1 /nobreak 
 echo Fetching from Git...
+"%CD%\scripts\git\bin\git.exe" gc
 "%CD%\scripts\git\bin\git.exe" reset origin/release --hard
 echo Script is up to date. Press any key to close.
 pause
