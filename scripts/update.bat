13825983c05301a28d8d65ade06cb6d4d8923442
Devil4ngle <isaakpocchiesa@gmail.com>
2023-12-26 10:37:45 +0100

    Update Release

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,8 +1,6 @@
 @echo off
-timeout /t 3 /nobreak 
+timeout /t 1 /nobreak 
 echo Fetching from Git...
-"%CD%\scripts\git\bin\git.exe" update-index --skip-worktree scripts/git/*
-"%CD%\scripts\git\bin\git.exe" fetch origin
-"%CD%\scripts\git\bin\git.exe" reset origin/release
+"%CD%\scripts\git\bin\git.exe" reset origin/release --hard
 echo Script is up to date. Press any key to close.
 pause
